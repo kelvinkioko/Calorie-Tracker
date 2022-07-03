b65566066fa567d6981fee663afb553da5f4251a
Kelvin Kioko <kelvinmkioko@gmail.com>
2022-07-03 09:11:23 +0300

    [gradle-kts] Upgraded to gradle kts upto base module

---
+++ buildSrc/src/main/java/DaggerHilt.kt
@@ -0,0 +1,5 @@
+object DaggerHilt {
+    const val hiltVersion = "2.40"
+    const val hiltAndroid = "com.google.dagger:hilt-android:$hiltVersion"
+    const val hiltCompiler = "com.google.dagger:hilt-android-compiler:$hiltVersion"
+}
