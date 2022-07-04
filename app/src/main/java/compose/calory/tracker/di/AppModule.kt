f4615502ad85aae2d2f3ef091cadc43c811d6416
Kelvin Kioko <kelvinmkioko@gmail.com>
2022-07-04 10:09:56 +0300

    [Age screen] Created the Age screen and view model. Added string resource manager sealed class to manage dynamic and string resources

--- app/src/main/java/compose/calory/tracker/di/AppModule.kt
+++ app/src/main/java/compose/calory/tracker/di/AppModule.kt
@@ -5,6 +5,7 @@ import android.content.Context.MODE_PRIVATE
 import android.content.SharedPreferences
 import compose.calory.core.data.preferences.DefaultPreferences
 import compose.calory.core.domain.preferences.Preferences
+import compose.calory.core.domain.use_case.FilterOutDigits
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -27,5 +28,11 @@ object AppModule {
     @Singleton
     fun providePreferences(sharedPrefs: SharedPreferences): Preferences {
         return DefaultPreferences(sharedPrefs = sharedPrefs)
+    }
+
+    @Provides
+    @Singleton
+    fun provideFilterOutDigitsUseCase(): FilterOutDigits {
+        return FilterOutDigits()
     }
 }
---
+++ onboarding/onboarding_presentation/src/main/java/compose/calory/onboarding_presentation/age/AgeScreen.kt
@@ -0,0 +1,89 @@
+package compose.calory.onboarding_presentation.age
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import compose.calory.core.domain.model.Gender
+import compose.calory.core.util.UiEvent
+import compose.calory.core_ui.localSpacing
+import compose.calory.onboarding_presentation.components.ActionButton
+import compose.calory.onboarding_presentation.components.SelectableButton
+import compose.calory.onboarding_presentation.components.UnitTextField
+import compose.calory.core.R as coreRes
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun DefaultAgeScreenPreview() {
+    // TODO: Add screen for preview
+}
+
+@Composable
+fun AgeScreen(
+    scaffoldState: ScaffoldState,
+    onNavigate: (UiEvent.Navigate) -> Unit,
+    viewModel: AgeViewModel = hiltViewModel()
+) {
+    val spacing = localSpacing.current
+    val context = LocalContext.current
+    LaunchedEffect(key1 = true) {
+        viewModel.uiEvent.collect { event ->
+            when (event) {
+                is UiEvent.Navigate -> onNavigate(event)
+                is UiEvent.ShowSnackBar -> {
+                    scaffoldState.snackbarHostState.showSnackbar(
+                        message = event.message.asString(context = context)
+                    )
+                }
+                else -> Unit
+            }
+        }
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(spacing.spaceLarge)
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = stringResource(id = coreRes.string.whats_your_age),
+                style = MaterialTheme.typography.h3
+            )
+            Spacer(modifier = Modifier.height(spacing.spaceMedium))
+            UnitTextField(
+                value = viewModel.age,
+                onValueChanged = viewModel::onAgeEntered,
+                unit = stringResource(id = coreRes.string.years)
+            )
+        }
+        ActionButton(
+            text = stringResource(id = coreRes.string.next),
+            onClick = viewModel::onNextClick,
+            modifier =  Modifier.align(Alignment.BottomEnd)
+        )
+    }
+}
+
